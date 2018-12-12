8a9e4f82c51a3af43ba03f6d4114a0dca6f71558
dakahn <dakahn@protonmail.com>
2018-12-12 13:55:33 -0600

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default () => (
+  <>
+    <h1>Carbon Accessibility Sandbox</h1>
+    <nav>
+      <ul>
+        <li>
+          <a href="/johnny">Johnny</a>
+        </li>
+        <li>
+          <a href="/joey">Joey</a>
+        </li>
+        <li>
+          <a href="/tommy">Tommy</a>
+        </li>
+        <li>
+          <a href="/deedee">DeeDee</a>
+        </li>
+        <li>
+          <a href="/marky">Marky</a>
+        </li>
+        <li>
+          <a href="/richie">Richie</a>
+        </li>
+      </ul>
+    </nav>
+  </>
+);
