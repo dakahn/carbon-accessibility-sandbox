cfd3f819d1cb73017c5acfecb5d24389fd2da013
dakahn <dakahn@protonmail.com>
2018-12-13 15:29:53 -0600

    Add component list and some a11y styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,25 +15,107 @@ export default () => (
           ♿
         </span>
       </H1>
-      <nav>
+      <nav aria-labelledby="component-label">
+        <H2 id="component-label">Components</H2>
         <Ul>
           <li>
-            <Link to="/johnny">Jackie is a punk</Link>
+            <Link to="/accordion">Accordion</Link>
           </li>
           <li>
-            <Link to="/joey">Judy is a runt</Link>
+            <Link to="/breadcrumb">Breadcrumb</Link>
           </li>
           <li>
-            <Link to="/tommy">They both went down to Berlin</Link>
+            <Link to="/button">Button</Link>
           </li>
           <li>
-            <Link to="/deedee">Joined the Ice Capades</Link>
+            <Link to="/checkbox">Checkbox</Link>
           </li>
           <li>
-            <Link to="/marky">and oh, I don't know why</Link>
+            <Link to="/code-snippet">Code Snippet</Link>
           </li>
           <li>
-            <Link to="/richie">Oh, I don't know why</Link>
+            <Link to="/content-switcher">Content Switcher</Link>
+          </li>
+          <li>
+            <Link to="/data-table">Data Table</Link>
+          </li>
+          <li>
+            <Link to="/date-picker">Date Picker</Link>
+          </li>
+          <li>
+            <Link to="/dropdown">Dropdown</Link>
+          </li>
+          <li>
+            <Link to="/file-uploader">File Uploader</Link>
+          </li>
+          <li>
+            <Link to="/form">Form</Link>
+          </li>
+          <li>
+            <Link to="/inline-loading">Inline Loading</Link>
+          </li>
+          <li>
+            <Link to="/link">Link</Link>
+          </li>
+          <li>
+            <Link to="/list">List</Link>
+          </li>
+          <li>
+            <Link to="/loading">Loading</Link>
+          </li>
+          <li>
+            <Link to="/loading">Loading</Link>
+          </li>
+          <li>
+            <Link to="/modal">Modal</Link>
+          </li>
+          <li>
+            <Link to="/notification">Notification</Link>
+          </li>
+          <li>
+            <Link to="/number-input">Number Input</Link>
+          </li>
+          <li>
+            <Link to="/overflow-menu">Overflow Menu</Link>
+          </li>
+          <li>
+            <Link to="/pagination">Pagination</Link>
+          </li>
+          <li>
+            <Link to="/progress-indicator">Progress Indicator</Link>
+          </li>
+          <li>
+            <Link to="/radio-button">Radio Button</Link>
+          </li>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+          <li>
+            <Link to="/select">Select</Link>
+          </li>
+          <li>
+            <Link to="/slider">Slider</Link>
+          </li>
+          <li>
+            <Link to="/structured-list">Structured List</Link>
+          </li>
+          <li>
+            <Link to="/tabs">Tabs</Link>
+          </li>
+          <li>
+            <Link to="/tag">Tag</Link>
+          </li>
+          <li>
+            <Link to="/text-input">Text Input</Link>
+          </li>
+          <li>
+            <Link to="/tile">Tile</Link>
+          </li>
+          <li>
+            <Link to="/toggle">Toggle</Link>
+          </li>
+          <li>
+            <Link to="/tooltip">Tooltip</Link>
           </li>
         </Ul>
       </nav>
@@ -42,7 +124,7 @@ export default () => (
 );
 
 const Main = styled.main`
-  margin-top: 4rem;
+  margin-top: 2rem;
   margin-left: 2rem;
 `;
 
@@ -51,12 +133,23 @@ const H1 = styled.h1`
   margin-bottom: 4rem;
 `;
 
+const H2 = styled.h2`
+  font-size: 2rem;
+  margin-bottom: 1rem;
+`;
+
 const Ul = styled.ul`
+  margin-left: 1.5rem;
+  width: 200px;
+
   li {
-    font-size: 1.5rem;
-    margin-bottom: 2rem;
+    font-size: 1.25rem;
+    margin-bottom: 1.5rem;
   }
+
   a:focus {
-    outline: 2px solid #5aaafa;
+    outline: 0;
+    color: black;
+    background-color: #41d6c3;
   }
 `;
