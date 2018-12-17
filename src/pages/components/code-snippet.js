197ea8af44f2f33a87c4e037bfcca203f3226c7a
dakahn <dakahn@protonmail.com>
2018-12-17 13:15:04 -0600

    Add a little padding to the code-snippet examples

--- src/pages/components/code-snippet.js
+++ src/pages/components/code-snippet.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import Layout from '../../components/layout';
 import Back from '../../components/back';
+import styled from 'styled-components';
 import CodeSnippet from 'carbon-components-react/lib/components/CodeSnippet/CodeSnippet';
 
 export default props => (
   <Layout>
     <Back to="/components">return to components</Back>
     <h2>Inline</h2>
-    <p>
+    <P>
       First do this{' '}
       <CodeSnippet type="inline">{`SELECT * FROM table;`}</CodeSnippet>
-    </p>
+    </P>
     <h2>Single line</h2>
-    <p>
+    <P>
       <CodeSnippet type="single">{`SELECT age, name FROM people;`}</CodeSnippet>
-    </p>
+    </P>
     <h2>Multi-line</h2>
-    <p>
+    <P>
       <CodeSnippet type="multi">
         {`UPDATE people SET name = 'Johnny', age = 101 WHERE 
         (name = 'Joey' AND age = 100) OR 
         name = 'DeeDee';`}
       </CodeSnippet>
-    </p>
+    </P>
   </Layout>
 );
+
+const P = styled.p`
+  margin-bottom: 2rem;
+`;
